25aaa1d8615a856dd76ba4f1f378700bf0d3716b
ddfkit <chovs.thai@gmail.com>
2019-07-07 10:23:21 +0900

    Update Test.php

--- Test.php
+++ Test.php
@@ -1,6 +1,7 @@
 <?php
-class test extend PHPUnit\Framework\TestCase{
+class test extend PHPUnit\Framework\TestCase
+{
     public function testOnePlusOne() {
-        $this->assertEquals(1+1, 1);
+        $this->assertEquals(1 + 1, 1);
     }
 }
